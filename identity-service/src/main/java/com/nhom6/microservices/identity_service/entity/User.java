f9550c1558a0ec29a08cdc6fd800877b8e722aad
4miby <huyvo6999@gmail.com>
2024-10-23 19:34:10 +0700

    Add securityConfig to authenticate

--- identity-service/src/main/java/com/nhom6/microservices/identity_service/entity/User.java
+++ identity-service/src/main/java/com/nhom6/microservices/identity_service/entity/User.java
@@ -1,25 +1,32 @@
 package com.nhom6.microservices.identity_service.entity;
 
+
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.HashSet;
+import java.util.Set;
+
+
 @Getter
 @Setter
-@Table(name = "user")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String email;
+    String id;
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    String email;
+
+    @ElementCollection
+    Set<String> roles;
 }
