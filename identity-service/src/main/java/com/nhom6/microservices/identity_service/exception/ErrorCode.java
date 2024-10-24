070af7727bf876770e87c69e6c8c2cf37b14fdc6
4miby <huyvo6999@gmail.com>
2024-10-24 17:31:56 +0700

    Quản lý Exception để trả về httpStatusCode

--- identity-service/src/main/java/com/nhom6/microservices/identity_service/exception/ErrorCode.java
+++ identity-service/src/main/java/com/nhom6/microservices/identity_service/exception/ErrorCode.java
@@ -3,24 +3,29 @@ package com.nhom6.microservices.identity_service.exception;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 public enum ErrorCode {
 
-    UNCATEGORIZED_EXCEPTION(400, "Uncategorized Exception"),
-    INVALID_KEY_EXCEPTION(1000, "Uncategorized Exception"),
-    USER_EXISTED(1001, "User existed"),
-    USERNAME_INVALID(1002, "Username must be at least 3 characters"),
-    INVALID_PASSWORD(1003, "Password must be at least 8 characters"),
-    USER_NOT_FOUND(1004, "User not found"),
-    UNAUTHENTICATED(1005, "Unauthenticated"),
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY_EXCEPTION(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(1005, "User not existed", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     ;
 
     int Code;
     String Message;
-    ErrorCode(int code, String message) {
+    HttpStatusCode httpStatusCode;
+    ErrorCode(int code, String message, HttpStatusCode httpStatusCode) {
         this.Code = code;
         this.Message = message;
+        this.httpStatusCode = httpStatusCode;
     }
 
 }
--- identity-service/src/main/java/com/nhom6/microservices/identity_service/exception/GlobalExceptionHandler.java
+++ identity-service/src/main/java/com/nhom6/microservices/identity_service/exception/GlobalExceptionHandler.java
@@ -1,22 +1,29 @@
 package com.nhom6.microservices.identity_service.exception;
 
 import com.nhom6.microservices.identity_service.dto.request.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.security.access.AccessDeniedException;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    // Xử lý tất cả Exception
     @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException exception) {
+    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception){
+        log.error("Exception: ", exception);
+        ApiResponse apiResponse = new ApiResponse();
 
-       ApiResponse apiResponse = new ApiResponse();
-       apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
-       apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
 
-       return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity.badRequest().body(apiResponse);
     }
+
+    // Xử lý Custom Exception
     @ExceptionHandler(value = AppException.class)
     ResponseEntity<ApiResponse> handleAppException(AppException exception) {
         ErrorCode errorCode = exception.getErrorCode();
@@ -25,26 +32,42 @@ public class GlobalExceptionHandler {
         apiResponse.setMessage(errorCode.getMessage());
         apiResponse.setCode(errorCode.getCode());
 
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity
+                .status(errorCode.getHttpStatusCode())
+                .body(apiResponse);
     }
+
+    // Xử lý Access Denied Exception
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception){
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(
+                ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build()
+        );
+    }
+    // Xử lý  MethodArgumentNotValidException Exception
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException exception){
         String enumKey = exception.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY_EXCEPTION;
-        try{
+
+        try {
             errorCode = ErrorCode.valueOf(enumKey);
-        }
-        catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e){
 
         }
-
 
         ApiResponse apiResponse = new ApiResponse();
-        apiResponse.setMessage(errorCode.getMessage());
-        apiResponse.setCode(errorCode.getCode());
-        return ResponseEntity.badRequest().body(apiResponse);
 
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 
 
