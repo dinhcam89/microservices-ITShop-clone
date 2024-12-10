57d51c29b10ea5edb6d0a805b49e9068f4589126
dcamm79 <dinhcam1512@gmail.com>
2024-12-11 01:15:30 +0700

    fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                     <span className="text-3xl text-indigo-600 mr-1 pt-2">
                         <ion-icon name="laptop-outline"></ion-icon>
                     </span>
-                    IT Shop
+                    IT Shop v0
                 </div>
                 <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
                     <ion-icon name={open ? 'close':'menu'}></ion-icon>
